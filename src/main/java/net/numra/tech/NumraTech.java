3a47ab209e384b06cceeecb95b38003237f69fe7
CoolandonRS <49355953+CoolandonRS@users.noreply.github.com>
2021-12-27 23:52:22 -0800

    Revert "Started conveyer_wood"
    This reverts commit 551715da0488cb5cd67a05e27ef82a909747e47e.

--- src/main/java/net/numra/tech/NumraTech.java
+++ src/main/java/net/numra/tech/NumraTech.java
@@ -1,17 +1,21 @@
 package net.numra.tech;
 
 import net.fabricmc.api.ModInitializer;
-import net.numra.tech.blocks.conveyor_basic;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class NumraTech implements ModInitializer {
-	public static final Logger logger_main = LogManager.getLogger("NumraTech");
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LogManager.getLogger("NumraTech");
 
 	@Override
 	public void onInitialize() {
-		logger_main.info("Hello world!");
-		conveyor_basic.init();
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
 	}
 }
