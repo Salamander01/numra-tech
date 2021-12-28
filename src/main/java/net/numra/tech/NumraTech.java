63954f06adeb988c5d540cf532f82e1acfdb2216
CoolandonRS <49355953+CoolandonRS@users.noreply.github.com>
2021-12-27 23:56:52 -0800

    Started conveyer_wood
    Made a textureless block called conveyer_wood in the redstone tab.
    Putting this in the PR so others can see how to do this :)

--- src/main/java/net/numra/tech/NumraTech.java
+++ src/main/java/net/numra/tech/NumraTech.java
@@ -1,21 +1,17 @@
 package net.numra.tech;
 
 import net.fabricmc.api.ModInitializer;
+import net.numra.tech.blocks.conveyor_basic;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
 public class NumraTech implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("NumraTech");
+	public static final Logger logger_main = LogManager.getLogger("NumraTech");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		logger_main.info("Hello world!");
+		conveyor_basic.init();
 	}
 }
