1b18e1fe65da188593cdc45a2bbfbd303e3244b0
CoolandonRS <49355953+CoolandonRS@users.noreply.github.com>
2021-12-29 22:19:33 -0800

    Did many things
    I added corner textures, revamped the blockstates of ConveyorBasicBlock by making ConveyorDirection, made states update along connected conveyors, and probably a bit more that I am forgetting.

--- src/main/java/net/numra/tech/blocks/ConveyorBasicBlock.java
+++ src/main/java/net/numra/tech/blocks/ConveyorBasicBlock.java
@@ -3,6 +3,7 @@ package net.numra.tech.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
@@ -11,18 +12,21 @@ import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 
-import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+
+import static net.numra.tech.blocks.ConveyorBasic.CONVEYOR_WOOD;
 
 public class ConveyorBasicBlock extends Block {
     public static final BooleanProperty ACTIVE = BooleanProperty.of("on");
-    EnumProperty<ConveyorDirection> DIRECTION = EnumProperty.of("direction", ConveyorDirection.class );
+    public static final EnumProperty<ConveyorDirection> DIRECTION = EnumProperty.of("direction", ConveyorDirection.class );
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
@@ -43,9 +47,14 @@ public class ConveyorBasicBlock extends Block {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         return switch (state.get(DIRECTION)) {
+            // Straight
             case NORTH, SOUTH -> VoxelShapes.union(Block.createCuboidShape(3, 1.5, 0, 13, 3.5, 16), Block.createCuboidShape(1, 0, 0, 3, 4, 16), Block.createCuboidShape(13, 0, 0, 15, 4, 16));
             case EAST, WEST -> VoxelShapes.union(Block.createCuboidShape(0, 1.5, 3, 16, 3.5, 13), Block.createCuboidShape(0, 0, 1, 16, 4, 3), Block.createCuboidShape(0, 0, 13, 16, 4, 15));
-            default -> VoxelShapes.fullCube(); // Will have to add cases for corners
+            // turn
+            case NORTH_EAST, WEST_SOUTH -> VoxelShapes.union(Block.createCuboidShape(13, 0, 13, 15, 4, 15), Block.createCuboidShape(15, 0, 13, 16, 4, 15), Block.createCuboidShape(13, 0, 15, 15, 4, 16), Block.createCuboidShape(13, 1.5, 3, 16, 3.5, 13), Block.createCuboidShape(3, 1.5, 13, 13, 3.5, 16), Block.createCuboidShape(3, 1.5, 3, 13, 3.5, 13), Block.createCuboidShape(1, 0, 1, 3, 4, 3), Block.createCuboidShape(3, 0, 1, 16, 4, 3), Block.createCuboidShape(1, 0, 3, 3, 4, 16));
+            case NORTH_WEST, EAST_SOUTH -> VoxelShapes.union(Block.createCuboidShape(1, 0, 13, 3, 4, 15), Block.createCuboidShape(1, 0, 15, 3, 4, 16), Block.createCuboidShape(0, 0, 13, 1, 4, 15), Block.createCuboidShape(3, 1.5, 13, 13, 3.5, 16), Block.createCuboidShape(0, 1.5, 3, 3, 3.5, 13), Block.createCuboidShape(3, 1.5, 3, 13, 3.5, 13), Block.createCuboidShape(13, 0, 1, 15, 4, 3), Block.createCuboidShape(13, 0, 3, 15, 4, 16), Block.createCuboidShape(0, 0, 1, 13, 4, 3));
+            case SOUTH_EAST, WEST_NORTH -> VoxelShapes.union(Block.createCuboidShape(13, 0, 1, 15, 4, 3), Block.createCuboidShape(13, 0, 0, 15, 4, 1), Block.createCuboidShape(15, 0, 1, 16, 4, 3), Block.createCuboidShape(3, 1.5, 0, 13, 3.5, 3), Block.createCuboidShape(13, 1.5, 3, 16, 3.5, 13), Block.createCuboidShape(3, 1.5, 3, 13, 3.5, 13), Block.createCuboidShape(1, 0, 13, 3, 4, 15), Block.createCuboidShape(1, 0, 0, 3, 4, 13), Block.createCuboidShape(3, 0, 13, 16, 4, 15));
+            case SOUTH_WEST, EAST_NORTH -> VoxelShapes.union(Block.createCuboidShape(1, 0, 1, 3, 4, 3),Block.createCuboidShape(0, 0, 1, 1, 4, 3), Block.createCuboidShape(1, 0, 0, 3, 4, 1), Block.createCuboidShape(0, 1.5, 3, 3, 3.5, 13), Block.createCuboidShape(3, 1.5, 0, 13, 3.5, 3), Block.createCuboidShape(3, 1.5, 3, 13, 3.5, 13), Block.createCuboidShape(13, 0, 13, 15, 4, 15), Block.createCuboidShape(0, 0, 13, 13, 4, 15), Block.createCuboidShape(13, 0, 0, 15, 4, 13));
         };
     }
 
@@ -58,6 +67,26 @@ public class ConveyorBasicBlock extends Block {
             case "west" -> this.getDefaultState().with(DIRECTION, ConveyorDirection.WEST);
             default -> this.getDefaultState();
         };
+    }
+
+    public boolean testConveyorConnect(BlockState outState, Direction direction, BlockState inState) {
+        //CREDIT: "! !#6008" in discord helped a lot with this, so thanks :)
+        if (direction.getOpposite() == outState.get(DIRECTION).getFirstDirection()) {
+            return outState.get(DIRECTION).getFirstDirection() == inState.get(DIRECTION).getSecondDirection();
+        } else if (direction == outState.get(DIRECTION).getSecondDirection()) {
+            return outState.get(DIRECTION).getSecondDirection() == inState.get(DIRECTION).getFirstDirection();
+        }
+        return false;
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (neighborState.getBlock() == CONVEYOR_WOOD) {
+            if (testConveyorConnect(state, direction, neighborState)) {
+                return state.with(ACTIVE, neighborState.get(ACTIVE));
+            }
+        }
+        return state;
     }
 
 
--- src/main/java/net/numra/tech/blocks/ConveyorDirection.java
+++ src/main/java/net/numra/tech/blocks/ConveyorDirection.java
@@ -1,26 +1,40 @@
 package net.numra.tech.blocks;
 
 import net.minecraft.util.StringIdentifiable;
+import net.minecraft.util.math.Direction;
 
 public enum ConveyorDirection implements StringIdentifiable {
-    NORTH("n"),
-    EAST("e"),
-    SOUTH("s"),
-    WEST("w"),
-    NORTH_EAST("ne"),
-    EAST_NORTH("en"),
-    SOUTH_EAST("se"),
-    EAST_SOUTH("es"),
-    SOUTH_WEST("sw"),
-    WEST_SOUTH("ws"),
-    NORTH_WEST("nw"),
-    WEST_NORTH("wn");
+    NORTH("north", Direction.NORTH,Direction.NORTH),
+    NORTH_EAST("north_east",Direction.NORTH,Direction.EAST),
+    EAST_NORTH("east_north",Direction.EAST,Direction.NORTH),
+    EAST("east",Direction.EAST,Direction.EAST),
+    SOUTH_EAST("south_east",Direction.SOUTH,Direction.EAST),
+    EAST_SOUTH("east_south",Direction.EAST,Direction.SOUTH),
+    SOUTH("south",Direction.SOUTH,Direction.SOUTH),
+    SOUTH_WEST("south_west",Direction.SOUTH,Direction.WEST),
+    WEST_SOUTH("west_south",Direction.WEST,Direction.SOUTH),
+    WEST("west",Direction.WEST,Direction.WEST),
+    NORTH_WEST("north_west",Direction.NORTH,Direction.WEST),
+    WEST_NORTH("west_north",Direction.WEST,Direction.NORTH);
 
-    ConveyorDirection(String name) {
+    ConveyorDirection(String lowName, Direction firstDirection, Direction secondDirection) {
+        this.lowName = lowName;
+        this.firstDirection = firstDirection;
+        this.secondDirection = secondDirection;
     }
 
+    private final Direction firstDirection;
+    private final Direction secondDirection;
+    private final String lowName;
+
+    public Direction getFirstDirection() {
+        return this.firstDirection;
+    }
+    public Direction getSecondDirection() {
+        return this.secondDirection;
+    }
     @Override
     public String asString() {
-        return this.name();
+        return this.lowName;
     }
 }
